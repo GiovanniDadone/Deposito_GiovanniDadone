0954f2ef2bc3005a4526c2616a6867af526d3865
GiovanniDadone <dadone.giovanni94@gmail.com>
2025-06-25 17:06:53 +0200

    funonziaaaa con gitkraken

---
+++ 25-06-2025/ProvaWhile.java
@@ -0,0 +1,12 @@
+public class ProvaWhile {
+    public static void main(String[] args) {
+        int count = 1;
+
+
+        //esempio while loop
+        while (count <= 5) {
+            count++;
+            System.out.println(count);
+        }
+    }
+}
