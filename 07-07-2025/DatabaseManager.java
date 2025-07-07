d5d51b999c5cbef28fc0018b8d391d6bf301247a
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-07 17:47:17 +0200

    fix metodi statici

--- 07-07-2025/DatabaseManager.java
+++ 07-07-2025/DatabaseManager.java
@@ -1,9 +1,11 @@
 public class DatabaseManager {
     private static DatabaseManager manager;
     private static int count = 0;
+    private static int id = 0;
 
-
-    private DatabaseManager() {}
+    private DatabaseManager() {
+        id++;
+    }
 
     public static DatabaseManager getInstance() {
         if (manager == null) {
@@ -15,10 +17,17 @@ public class DatabaseManager {
     public void connect() {
         count++;
         System.out.println("Connessione stabilita. Connessioni attive: " + getCount());
+    }
 
+    public void close() {
+        manager = null;
     }
 
     public int getCount() {
         return count;
+    }
+
+    public int getId() {
+        return id;
     }
 }
--- 07-07-2025/Main.java
+++ 07-07-2025/Main.java
@@ -12,12 +12,22 @@ public class Main {
         db2.connect();
         db2.connect();
 
-        // controllo che siano della stessa istanza
-        if (db1 == db2) {
+        // controllo che siano della stessa istanza tramite Id
+        if (db1.getId() == db2.getId()) {
             System.out.println("Conferma: db1 e db2 sono la stessa istanza.");
         } else {
             System.out.println("Errore: le istanze non coincidono.");
         }
+
+        System.out.println("ID uguali: " + db2.getId());
+
+        //se chiudo uno dei due, automaticamente dovrebbero chiudersi entrambi
+        db1.close();
+
+        //nel richiamare nuovamente getInstance() dovrei notare un incremento di Id in ambedue le variabili db1 e db2
+        DatabaseManager.getInstance();
+        System.out.println(db1.getId());
+        System.out.println(db2.getId());
 
         // print delle connessioni attive
         System.out.println(db1.getCount());
