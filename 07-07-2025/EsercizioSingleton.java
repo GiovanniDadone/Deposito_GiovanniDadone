8b31fc72d3712b08020e447b2743f0e6be7a3644
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-07 16:15:28 +0200

    final patch

--- 07-07-2025/EsercizioSingleton.java
+++ 07-07-2025/EsercizioSingleton.java
@@ -1,8 +1,10 @@
 public class EsercizioSingleton {
     public static void main(String[] args) {
+        Logger.getInstance().scriviMessaggio("Vai! Colpo di formazione combinato: Esplosione dei Campi Flegrei!");
+        Logger.getInstance().scriviMessaggio("Cuore del Gigante, Fermalo!");
         System.out.println("Controlliamo se sono lo stesso oggetto");
-        System.out.println(Logger.getInstance().equals(Logger.getInstance()));
-        System.out.println(Logger.getInstance()==Logger.getInstance());
+        System.out.println(Logger.getInstance().equals(Logger.getInstance()) + " con equals()");
+        System.out.println((Logger.getInstance()==Logger.getInstance()) + "con il double equals \"==\"");
         //in entrambi i casi stampa true quindi sono lo stesso oggetto chiamato in due momenti diversi
     }
 }
