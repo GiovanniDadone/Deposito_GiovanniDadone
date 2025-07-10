ad87d6a46f1836949c041e8b3817a54060f805f6
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-10 15:30:16 +0200

    Create EsercizioFactory2.java

---
+++ 10-07-2025/EsercizioFactory2.java
@@ -0,0 +1,125 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class EsercizioFactory2 {
+    public static void main(String[] args) {
+        Scanner intScanner = new Scanner(System.in);
+        boolean uscita = false;
+
+        while (!uscita) {
+            MenuFacade.displayMenu();
+            int scelta = 0;
+            while (scelta == 0) {
+                try {
+                    scelta = intScanner.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Non è un numero riprova");
+                    intScanner.nextLine();
+                    scelta = 0;
+                }
+            }
+            MenuFacade.sceltaForma(scelta);
+            if (scelta == 3) {
+                break;
+            }
+            System.out.println();
+
+        }
+        intScanner.close();
+    }
+}
+
+// 1. Product: definisce l'interfaccia del prodotto
+interface IShape {
+    void disegna();
+}
+
+// 2. ConcreteProductA: implementa Product
+class Circle implements IShape {
+    @Override
+    public void disegna() {
+        System.out.println("    ***    ");
+        System.out.println("  *     *  ");
+        System.out.println(" *       * ");
+        System.out.println(" *       * ");
+        System.out.println("  *     *  ");
+        System.out.println("    ***    ");
+    }
+}
+
+// 3. ConcreteProductB: un altro prodotto concreto
+class Square implements IShape {
+    @Override
+    public void disegna() {
+        System.out.println("********");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("********");
+    }
+}
+
+// ShapeCreator: dichiara il Factory Method
+abstract class ShapeCreator {
+    // Factory Method: restituisce un Product
+    public abstract IShape creaForma();
+
+    // Un metodo del creator che utilizza il prodotto
+    public void disegna() {
+        IShape product = creaForma(); // creazione del prodotto
+        product.disegna(); // uso del prodotto
+    }
+}
+
+// implementa disegna forma
+class CircleFactory extends ShapeCreator {
+    @Override
+    public Circle creaForma() {
+        return new Circle();
+    }
+}
+
+// implementa disegnaFroma()
+class SquareFactory extends ShapeCreator {
+    @Override
+    public Square creaForma() {
+        return new Square();
+    }
+}
+
+// facade Menu + strategy con direttamente il subject
+class MenuFacade {
+    private static ShapeCreator creator;
+
+    public static void displayMenu() {
+        System.out.println("=========MENU DELLE FORME===========");
+        System.out.println("Che forma vuoi disegnare?");
+        System.out.println("1. Cerchio");
+        System.out.println("2. Quadrato");
+        System.out.print("Scelta: ");
+    }
+
+    public static void sceltaForma(int scelta) {
+
+        switch (scelta) {
+            case 1:
+                setStrategy(new CircleFactory());
+                creator.disegna(); // Usa Circle Factory
+                break;
+            case 2:
+                setStrategy(new SquareFactory());
+                creator.disegna(); // Usa Circle Factory
+                break;
+
+            default:
+                System.out.println("Scelta non valida");
+                break;
+        }
+
+    }
+
+    private static void setStrategy(ShapeCreator newCreator) {
+        creator = newCreator;
+    }
+}
