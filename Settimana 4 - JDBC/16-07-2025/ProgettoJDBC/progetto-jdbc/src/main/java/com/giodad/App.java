11b869c005f64516ed0cc9140cfede4d9c79484e
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-16 16:52:14 +0200

    inizio JDBC

--- Settimana 4 - JDBC/16-07-2025/ProgettoJDBC/progetto-jdbc/src/main/java/com/giodad/App.java
+++ Settimana 4 - JDBC/16-07-2025/ProgettoJDBC/progetto-jdbc/src/main/java/com/giodad/App.java
@@ -1,7 +1,9 @@
 package com.giodad;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -33,6 +35,28 @@ public class App {
     }
 
     public static void main(String[] args) {
-        
+        String DB_DRIVER = "com.mysql.jdbc.Driver";
+        String URL = "jdbc:mysql://localhost:3306/giovannidb";
+        String USERNAME = "giovanni";
+        String PASSWORD = "B@RGHa86$b";
+
+        // enuncia la connessione
+        Connection connection = null;
+
+        try {
+            // Registra il Driver (solo se non caricato con Maven)
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            if(connection != null) {
+                System.out.println("Successfully connected");
+            } else {
+                System.out.println("Failed to connect");
+            }
+
+            Statement stmt = connection.createStatement();
+            ResultSet set = stmt.executeQuery(""/*<-----inserire query */);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
