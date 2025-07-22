648efb01fc01f85a018c74ed8ccff353473de951
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-22 16:53:26 +0200

    update progetto

--- Settimana 5 - Spring Boot/22-05-2025/progetto-utente-todo/src/main/java/com/giodad/progetto_utente_todo/model/Todo.java
+++ Settimana 5 - Spring Boot/22-05-2025/progetto-utente-todo/src/main/java/com/giodad/progetto_utente_todo/model/Todo.java
@@ -1,34 +1,44 @@
 package com.giodad.progetto_utente_todo.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
-@Entity
+@AllArgsConstructor
+@Builder
 @Table(name = "todo")
 public class Todo {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    
-    @Column(name = "descrizione", nullable = false, length = 100)
-    private String descrizione;
-    private boolean completato;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
 
-    @Autowired
-    public Todo(String descrizione, boolean completato) {
-        this.descrizione = descrizione;
-        this.completato = completato;
-    }
+@Column(nullable = false)
+private String descrizione;
+
+private boolean completato;
+
+@ManyToOne
+@JoinColumn(name = "utente_id", nullable = false)
+private Utente utente;
+
+@OneToMany(mappedBy = "todo", cascade = CascadeType.ALL, orphanRemoval = true)
+private List<Commento> commenti;
 }
 
