566baae9242b0ce992f6318517d5240b56f1f466
GiovanniDadone <143901741+GiovanniDadone@users.noreply.github.com>
2025-07-22 14:30:20 +0200

    fix v2

--- Settimana 5 - Spring Boot/22-05-2025/progetto-todo/src/main/java/com/giodad/progetto_todo/repository/TodoRepository.java
+++ Settimana 5 - Spring Boot/22-05-2025/progetto-todo/src/main/java/com/giodad/progetto_todo/repository/TodoRepository.java
@@ -7,5 +7,19 @@ import com.giodad.progetto_todo.model.Todo;
 
 @Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
-    //tutti i metodi crud base già pronti!
+    // tutti i metodi crud base già pronti!
+    // ****I METODI SONO I SEGUENTI*****
+    /*------------------------------------- */
+    // <S extends T> S save(S entity);
+    // <S extends T> Iterable<S> saveAll(Iterable<S> entities);
+    // Optional<T> findById(ID id);
+    // boolean existsById(ID id);
+    // Iterable<T> findAll();
+    // Iterable<T> findAllById(Iterable<ID> ids);
+    // long count();
+    // void deleteById(ID id);
+    // void delete(T entity);
+    // void deleteAllById(Iterable<? extends ID> ids);
+    // void deleteAll(Iterable<? extends T> entities);
+    // void deleteAll();
 }
